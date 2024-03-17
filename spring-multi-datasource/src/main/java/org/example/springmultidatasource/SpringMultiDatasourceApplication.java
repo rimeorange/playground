7baf4d5ef2a00d31b27d058cfff6e938dbacc408
rimeorange <rimeorange83@gmail.com>
2024-03-17 20:20:56 +0900

    init commit

---
+++ spring-multi-datasource/src/main/java/org/example/springmultidatasource/SpringMultiDatasourceApplication.java
@@ -0,0 +1,13 @@
+package org.example.springmultidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultiDatasourceApplication.class, args);
+    }
+
+}
---
+++ spring-multi-datasource/src/test/java/org/example/springmultidatasource/SpringMultiDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springmultidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
