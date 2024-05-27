0232a67243f23f14013f2b600c23de6d14fce5cd
rimeorange <rimeorange83@gmail.com>
2024-05-27 22:47:34 +0900

    feat : data-multisource kotlin init commit

---
+++ spring-multi-datasource_withkotlin/src/test/kotlin/org/example/multidatasource/SpringMultiDatasourceWithkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.multidatasource
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMultiDatasourceWithkotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
